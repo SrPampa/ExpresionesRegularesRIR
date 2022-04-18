1d499207411c2f6ec066930cc0307ad17c13e642
Ruth.H <ruth.herqui@educa.jcyl.es>
2022-04-18 20:28:57 +0200

    CARPETA TEXTOS PARA METER LOS TEXTOS Y EL WORD!!

--- src/main/java/com/venancio/dam/ExprRegulRIR/Password.java
+++ src/main/java/com/venancio/dam/ExprRegulRIR/Password.java
@@ -6,18 +6,22 @@ import java.util.regex.Pattern;
 public class Password {
 
 	public static void main(String[] args) {
-		
-		String password = "holaaa3aAaa[aaaaa;\n" + "holla2a;" + "\nhola32a3;\n" + "eli__aa;" + "\n_sara;\n";
-		
-		
-		//Contraseña segura
-		Pattern patron = Pattern.compile("(^|\\n)(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}\\[\\]:;<>,.?/~_+-=|\\\\]).{8,32}(;|$)");
+
+		String password = "ranansciftem;\r\n" + "mICkE;\r\n" + "B@AlVGz0e&N89h7R!$&m;\r\n" + "*@wScajY%rwAkKKAUKFZ;\r\n"
+				+ "w85#Fz&#XD52;\r\n" + "eAHEylaybTHh;\r\n" + "4TKpS8nBPTqd;\r\n" + "ToNYAk;\r\n" + "apsIOcOs;\r\n"
+				+ "iseyered;\r\n" + "2zhHue2H;\r\n" + "T!JV@Mwj;\r\n" + "uQt%@@$$g;\r\n" + "NBT8SJF^Q;\r\n"
+				+ "nitoNeRIK;\r\n" + "z^qM%fDZ3;\r\n";
+
+		// Contraseña segura
+		Pattern patron = Pattern.compile(
+				"(^|\\n)(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}\\[\\]:;<>,.?/~_+-=|\\\\]).{8,32}(;|$)");
 		Matcher mat = patron.matcher(password);
+		int contador = 0;
 		while (mat.find()) {
 			System.out.println(mat.group() + " posicion inicial: " + mat.start() + " posicion final: " + mat.end());
-		
+			contador++;
 		}
-		
-		
+
+		System.out.println("\nHay " + contador + " contraseñas seguras");
 	}
 }
--- src/main/java/com/venancio/dam/ExprRegulRIR/Username.java
+++ src/main/java/com/venancio/dam/ExprRegulRIR/Username.java
@@ -7,7 +7,20 @@ public class Username {
 
 	public static void main(String[] args) {
 
-		String nombre = "holaaa;\n" + "holla2a;" + "\nhola32a3;\n" + "eli__aa;" + "\n_sara;\n";
+		String nombre = "Acabtar;\r\n" + "luci;\r\n" + "almario;\r\n" + "12Altimpo_;\r\n" + "AwareIssue3;\r\n"
+				+ "Ayadorma_;\r\n" + "bajelar;\r\n" + "-Borrob;\r\n" + "Caguado;\r\n" + "capoSilly;\r\n" + "Execil;\r\n"
+				+ "_mor_;\r\n" + "FamousWeb;\r\n" + "freakThesoy;\r\n" + "1Guarave;\r\n" + "interest-Maxi;\r\n"
+				+ "issca;\r\n" + "Issuero;\r\n" + "Jame5Life;\r\n" + "Katrif;\r\n" + "Larmente;\r\n" + "Matterse;\r\n"
+				+ "Merodin;\r\n" + "monicast;\r\n" + "mowerBlacken;\r\n" + "Oleoran;\r\n" + "Opatisaj;\r\n"
+				+ "paradome;\r\n" + "PureEatsyou;\r\n" + "Sizz-lme;\r\n" + "SkillfulAra;\r\n" + "StormBoardin;\r\n"
+				+ "Aboutar;\r\n" + "Agotalec;\r\n" + "-Alcogogo;\r\n" + "7_Alidador;\r\n" + "Asiadora;\r\n"
+				+ "Bandit:Fraser;\r\n" + "CarLogic;\r\n" + "chronota;\r\n" + "cistMans;\r\n" + "crux;\r\n"
+				+ "Day.Zippo;\r\n" + "Despario;\r\n" + "_Donald-Magazine;\r\n" + "Doo mor;\r\n" + "Esmecie24;\r\n"
+				+ "fly12Uber;\r\n" + "Isagoll;\r\n" + "_Issuela;\r\n" + "IssueXglossy;\r\n" + "knotra_;\r\n"
+				+ "lastingNice;\r\n" + "25ProNewscast;\r\n" + "Sakilo;\r\n" + "Slashon;\r\n" + "marius;\r\n"
+				+ "Soca:jeme;\r\n" + "Time.Man;\r\n" + "Toxi-cam2;\r\n" + "TruckThega;\r\n" + "WalkerWeare;\r\n"
+				+ "With_painci;\r\n" + "wub__bato;";
+
 		int contador = 0;
 
 		Pattern patron = Pattern.compile("(^|\\n)[a-zA-Z0-9]([_-]|[a-zA-Z0-9]){3,18}[a-zA-Z0-9](;|$)");
